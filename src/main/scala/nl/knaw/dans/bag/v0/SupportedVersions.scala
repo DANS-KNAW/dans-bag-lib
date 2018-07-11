da2bdc942d5039bf43ebfcfe4015c85c7c7bdac3
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-07-11 17:58:14 +0200

    no support for v0.93 and v0.95 in v0 of the bag

--- src/main/scala/nl/knaw/dans/bag/v0/SupportedVersions.scala
+++ src/main/scala/nl/knaw/dans/bag/v0/SupportedVersions.scala
@@ -4,8 +4,6 @@ import gov.loc.repository.bagit.domain.Version
 
 object SupportedVersions {
 
-  val Version_0_93 = new Version(0, 93)
-  val Version_0_95 = new Version(0, 95)
   val Version_0_97 = new Version(0, 97)
   val Version_1_0 = new Version(1, 0)
 }
